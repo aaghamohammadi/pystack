86ac25cece5323749b7ea3c422a927d38b2831fd
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-06 06:19:35 +0430

    test: add tests for the Stack class

--- test_stack.py
+++
@@ -1,6 +0,0 @@
-def test_empty_stack():
-    assert True
-
-
-def test_push_one_element():
-    assert True
---
+++ tests/test_stack.py
@@ -0,0 +1,62 @@
+import pytest
+
+from pystack.stack import Stack
+
+
+class StackTest:
+    def setup_method(self):
+        self.st = Stack()
+
+    def test_stack_is_empty(self):
+        assert self.st.is_empty()
+        assert len(self.st) == 0
+        assert repr(self.st) == "Stack()"
+        assert str(self.st) == "Stack()"
+        with pytest.raises(IndexError):
+            self.st.pop()
+
+    def test_push_one_item(self):
+        self.st.push(2)
+        assert not self.st.is_empty()
+        assert len(self.st) == 1
+        assert self.st.top() == 2
+        assert repr(self.st) == "Stack()"
+        assert str(self.st) == "Stack(2)"
+
+    def test_push_multiple_items(self):
+        self.st.push(5)
+        self.st.push(3)
+        self.st.push(7)
+        assert len(self.st) == 3
+        assert self.st.top() == 7
+        assert str(self.st) == "Stack(5, 3, 7)"
+
+    def test_push_multiple_items_until_exception_raises(self):
+        for i in range(10):
+            self.st.push(i)
+        assert self.st.is_full()
+        with pytest.raises(OverflowError):
+            self.st.push(23)
+
+    def test_pop_one_item(self):
+        self.st.push(5)
+        top = self.st.pop()
+        assert top == 5
+        assert self.st.is_empty()
+        assert len(self.st) == 0
+
+    def test_pop_multiple_items(self):
+        self.st.push(3)
+        self.st.push(7)
+        assert self.st.pop() == 7
+        assert self.st.pop() == 3
+        with pytest.raises(IndexError):
+            self.st.pop()
+
+    def test_push_non_integer_items(self):
+        with pytest.raises(TypeError):
+            self.st.push("3")
+        with pytest.raises(TypeError):
+            self.st.push(3.1)
+        with pytest.raises(TypeError):
+            self.st.push(True)
