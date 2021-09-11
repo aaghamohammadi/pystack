61b7266d69a8780179fa3997266da1573ab23d21
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2021-09-11 09:09:17 +0430

    build: create setup.py for pystack library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pystack",
+    version="1.0.0",
+    description="Python implementation of a stack that uses Pytest as a test framework",
+    author="Alireza Aghamohammadi",
+    author_email="al.aghamohammadi@gmail.com",
+    url="https://github.com/aaghamohammadi/pystack",
+    packages=find_packages(),
+)
